434821e47707a5f4467464e2cb336839a787ed60
eros.pedrini <eros.pedrini@lumatagroup.com>
2015-04-21 09:43:22 +0200

    makeCacheMatrix first implementation

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,17 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    inversematrix <- NULL
+    set <- function(y) {
+        x <<- y
+        inversematrix <<- NULL
+    }
+    get <- function() x
+    setinverse <- function(inverse) inversematrix <<- inverse
+    getinverse <- function() inversematrix
+    list(set = set, get = get,
+         setinverse = setinverse,
+         getinverse = getinverse)
 }
 
 
